4637ef34a4cc61cf36ff7ed8d3d7e073850eed4b
SaulSandovalM <sauldevelop@gmail.com>
2017-08-28 13:16:05 -0500

    video

--- src/components/Bienvenida/Bienvenido.js
+++ src/components/Bienvenida/Bienvenido.js
@@ -3,7 +3,7 @@ import {StyleSheet, Text, View, Image} from 'react-native';
 import Video from 'react-native-video';
 import {Button} from 'native-base';
 import {Actions} from 'react-native-router-flux';
-import video from '../../assets/video/video1.mp4';
+import video from '../../assets/video/video2.mp4';
 import img from '../../assets/imgs/planb.png';
 
 class Bienvenido extends Component {
